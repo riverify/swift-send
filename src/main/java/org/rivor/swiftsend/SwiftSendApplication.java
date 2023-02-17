77d0ce47b1937bccaa0067ba9a5d1818f62f95c1
Riverify <halo@ahnu.edu.cn>
2023-02-17 16:14:06 +0800

    feature分支

--- src/main/java/org/rivor/swiftsend/SwiftSendApplication.java
+++ src/main/java/org/rivor/swiftsend/SwiftSendApplication.java
@@ -4,8 +4,6 @@ import lombok.extern.slf4j.Slf4j;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.web.servlet.ServletComponentScan;
-import org.springframework.scheduling.annotation.EnableScheduling;
-import org.springframework.scheduling.annotation.Scheduled;
 
 
 @SpringBootApplication
